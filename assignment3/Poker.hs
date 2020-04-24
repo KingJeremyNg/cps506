74df1a39d61c302178dcd4ee2ba4a6e124ee8bc1
kingjeremyng <jeremy97ng@gmail.com>
2020-04-24 15:08:08 -0400

    Making progress on haskell final

--- assignment3/Poker.hs
+++ assignment3/Poker.hs
@@ -5,13 +5,16 @@ module Poker where
     import System.Random
 
     -- Deal cards into 2 hands
-    separate list = ([x | x <- list, x `mod` 2 == 0], [x | x <- list, x `mod` 2 == 1])
+    separate list 0 (hand1, hand2) = (hand1, hand2)
+    separate (listH: listT) n (hand1, hand2)
+        | n `mod` 2 == 0 = separate listT (n - 1) (hand1 ++ [listH], hand2)
+        | n `mod` 2 == 1 = separate listT (n - 1) (hand1, hand2 ++ [listH])
 
     -- Get the card values of hand
     cardValue list = sort [(x - 1) `mod` 13 | x <- list]
 
     -- Get the suit values of hand
-    suitValue list = sort [(x - 1) `div` 13 | x <- list]
+    suitValue list = [(x - 1) `div` 13 | x <- list]
 
     -- Find all occurrences of a card in hand
     occurrences list item = length [x | x <- list, x == item]
@@ -25,25 +28,78 @@ module Poker where
     -- Converting hand into string list
     stringify [] _ list = list
     stringify (handH: handT) (suitH: suitT) list
-        | suitH == 0 = stringify handT suitT (concat [list, [show handH ++ "C"]])
-        | suitH == 1 = stringify handT suitT (concat [list, [show handH ++ "D"]])
-        | suitH == 2 = stringify handT suitT (concat [list, [show handH ++ "H"]])
-        | suitH == 3 = stringify handT suitT (concat [list, [show handH ++ "S"]])
+        | suitH == 0 = stringify handT suitT (concat [list, [show (handH + 1) ++ "C"]])
+        | suitH == 1 = stringify handT suitT (concat [list, [show (handH + 1) ++ "D"]])
+        | suitH == 2 = stringify handT suitT (concat [list, [show (handH + 1) ++ "H"]])
+        | suitH == 3 = stringify handT suitT (concat [list, [show (handH + 1) ++ "S"]])
 
-    -- THIS NEEDS WORK
-    -- evaluate (hand1, suit1) (hand2, suit2)
-    --     | royalFlush hand1 == True && royalFlush hand2 == False = stringify hand1 suit1 []
-    --     | royalFlush hand1 == False && royalFlush hand2 == True = stringify hand2 suit2 []
-    --     | straightFlush hand1 == True && straightFlush hand2 == False = stringify hand1 suit1 []
-    --     | straightFlush hand1 == False && straightFlush hand2 == True = stringify hand1 suit1 []
-    --     | otherwise = "TIE"
+    -- *******************************************
 
-    royalFlush hand =
-        case hand of
-            [0, 9, 10, 11, 12] -> True
-            [_, _, _, _, _] -> False
+    -- DETECTION FUNCTIONS
+    hasFlush hand
+        | occurrences (suitValue hand) 0 == 5 = True
+        | occurrences (suitValue hand) 1 == 5 = True
+        | occurrences (suitValue hand) 2 == 5 = True
+        | occurrences (suitValue hand) 3 == 5 = True
+        | otherwise = False
     
-    -- deal list = evaluate (sort list)
+    hasStraight (handH: handT)
+        | handT == [] = True
+        | head (handT) == handH + 1 = hasStraight handT
+        | otherwise = False
+
+    findDuplicate [] _ dupe = dupe
+    findDuplicate (handH: handT) runningList dupe
+        | has runningList handH == True = findDuplicate (handT) runningList (dupe ++ [handH])
+        | otherwise = findDuplicate (handT) (runningList ++ [handH]) dupe
+    
+    compareCards hand1 hand2 [] [] = []
+    compareCards hand1 hand2 (x: xs) (y: ys)
+        | x == y = compareCards hand1 hand2 xs ys
+        | x > y = hand1
+        | x < y = hand2
+
+    detectHighCard hand1 hand2
+        | head (sort hand1) == 1 && head(sort hand2) /= 1 = hand1
+        | head (sort hand1) /= 1 && head(sort hand2) == 1 = hand2
+        | otherwise = compareCards hand1 hand2 (reverse (sort hand1)) (reverse (sort hand2))
+    
+    -- *******************************************
+
+    -- Main deal function
+    deal list = start (separate list 10 ([], []))
+
+    -- Start of the evaluation process
+    start (hand1, hand2) = highCard hand1 hand2
+
+    highCard hand1 hand2
+        | detectHighCard hand1 hand2 == hand1 = hand1
+        | detectHighCard hand1 hand2 == hand2 = hand2
+        | otherwise = "tie"
+
+    -- -- Find winner for high card scenario, then move onto pair
+    -- highCard data1 data2 [] [] [] [] winner = pair data1 data2 (fst data1) (snd data2) (fst data1) (snd data2) winner
+    -- highCard data1 data2 (hand1H: hand1T) (suit1H: suit1T) (hand2H: hand2T) (suit2H: suit2T) winner
+    --     -- Check ace cards first
+    --     | hand1H == 0 && hand2H /= 0 = pair data1 data2 (fst data1) (snd data2) (fst data1) (snd data2) "hand1"
+    --     | hand1H /= 0 && hand2H == 0 = pair data1 data2 (fst data1) (snd data2) (fst data1) (snd data2) "hand2"
+    --     | hand1H == 0 && hand2H == 0 && suit1H > suit2H = highCard data1 data2 hand1T suit1T hand2T suit2T "hand1"
+    --     | hand1H == 0 && hand2H == 0 && suit1H < suit2H = highCard data1 data2 hand1T suit1T hand2T suit2T "hand2"
+    --     -- If card is equal then check suit
+    --     | hand1H == hand2H && suit1H > suit2H = highCard data1 data2 hand1T suit1T hand2T suit2T "hand1"
+    --     | hand1H == hand2H && suit1H < suit2H = highCard data1 data2 hand1T suit1T hand2T suit2T "hand2"
+    --     -- Compare 2 cards
+    --     | hand1H > hand2H = pair data1 data2 hand1T suit1T hand2T suit2T "hand1"
+    --     | hand1H < hand2H = pair data1 data2 hand1T suit1T hand2T suit2T "hand2"
+    --     -- | otherwise = highCard data1 data2 hand1T suit1T hand2T suit2T winner
+    
+    -- Find all pairs, then move onto 3 of a kind
+    -- pair data1 data2 [] [] [] [] winner = threeKind data1 data2 (fst data1) (snd data2) (fst data1) (snd data2) winner
+    -- pair data1 data2 (hand1H: hand1T) (suit1H: suit1T) (hand2H: hand2T) (suit2H: suit2T) winner
+    --     | occurrences (fst data1) 0 == 2 = "ok" --
+    --     | otherwise = pair data1 data2 hand1T suit1T hand2T suit2T winner
+
+    -- threeKind data1 data2 (hand1H: hand1T) (suit1H: suit1T) (hand2H: hand2T) (suit2H: suit2T) winner = winner ++ show ([x + 1 | x <- fst data1]) ++ show ([x + 1 | x <- fst data2])
 
     {-
     royal flush
--- final/Cauchy.hs
+++ final/Cauchy.hs
@@ -2,12 +2,51 @@ module Cauchy where
 
     data CauchyList = CauchyList Int [Int]
 
+    compareList (list1H: list1T) (list2H: list2T)
+        | list1H == list2H && ((list1T == []) || (list2T == [])) = True
+        | list1H == list2H = compareList list1T list2T
+        | otherwise = False
+    
+    addList list1 list2
+        | (length list1) == (length list2) = zipWith (+) list1 list2
+        | (length list1) < (length list2) = zipWith (+) (extendList list1 (length list2)) list2
+        | (length list1) > (length list2) = zipWith (+) list1 (extendList list2 (length list1))
+
+    extendList list n
+        | (length list == n) = list
+        | (length list < n) = extendList (list ++ [0]) n
+    
+    subtractList list1 list2
+        | (length list1) == (length list2) = zipWith (-) list1 list2
+        | (length list1) < (length list2) = zipWith (-) (extendList list1 (length list2)) list2
+        | (length list1) > (length list2) = zipWith (-) list1 (extendList list2 (length list1))
+    
+    multiplyList list1 list2 = [0]
+
+    -- ==
     instance Eq CauchyList where
-        -- Implement Eq type class here
+        CauchyList num1 list1 == CauchyList num2 list2
+            | num1 == num2 && compareList list1 list2 == True = True
+            | otherwise = False
 
+    -- +, -, *, abs, sigNum, fromInt
     instance Num CauchyList where
-        -- Implement Num type class here
-        
-    instance Show CauchyList where
-        -- Implement Show type class here
+        CauchyList num1 list1 + CauchyList num2 list2 =
+            CauchyList num1 [x `mod` num1 | x <- addList list1 list2]
 
+        CauchyList num1 list1 - CauchyList num2 list2 =
+            CauchyList num1 [x `mod` num1 | x <- subtractList list1 list2]
+
+        CauchyList num1 list1 * CauchyList num2 list2 =
+            CauchyList num1 [x `mod` num1 | x <- multiplyList list1 list2]
+        -- CauchyList num list * fromInteger scalar = CauchyList num [x * scalar | x <- list]
+        -- abs CauchyList num1 list1
+        -- signum CauchyList num1 list1
+        -- -- Takes number turn into cauchy
+
+        fromInteger n = CauchyList (fromIntegral n) [fromIntegral n]
+    
+    -- Give a string that mimics the output. String representation
+    instance Show CauchyList where
+        show (CauchyList num list) =
+            "p: " ++ show num ++ "\nlength: " ++ show (length list) ++ "\ncontent: " ++ show list
