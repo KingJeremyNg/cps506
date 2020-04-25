f821ae26ced0e3086e58cfbb2dd2592368b36b27
KingJeremyNg <jeremy97ng@gmail.com>
2020-04-24 22:32:48 -0400

    Finished Rust Assignment

--- final/Cauchy.hs
+++ final/Cauchy.hs
@@ -6,7 +6,7 @@ module Cauchy where
         | list1H == list2H && ((list1T == []) || (list2T == [])) = True
         | list1H == list2H = compareList list1T list2T
         | otherwise = False
-    
+
     addList list1 list2
         | (length list1) == (length list2) = zipWith (+) list1 list2
         | (length list1) < (length list2) = zipWith (+) (extendList list1 (length list2)) list2
@@ -15,20 +15,20 @@ module Cauchy where
     extendList list n
         | (length list == n) = list
         | (length list < n) = extendList (list ++ [0]) n
-    
+
     subtractList list1 list2
         | (length list1) == (length list2) = zipWith (-) list1 list2
         | (length list1) < (length list2) = zipWith (-) (extendList list1 (length list2)) list2
         | (length list1) > (length list2) = zipWith (-) list1 (extendList list2 (length list1))
-    
+
     multiplyList list1 list2
         | (length list1) == (length list2) = multiplyListCalculate (tail list1) (tail list2) [head list1] [head list2] []
         | otherwise = multiplyList (extendList list1 ((length list1) + (length list2))) (extendList list2 ((length list1) + (length list2)))
-    
+
     multiplyListCalculate (x: xs) (y: ys) list1 list2 answer
         | xs == [] && ys == [] = (answer ++ [(sum (zipWith (*) list1 (reverse list2)))])
         | otherwise = multiplyListCalculate xs ys (list1 ++ [x]) (list2 ++ [y]) (answer ++ [(sum (zipWith (*) list1 (reverse list2)))])
-    
+
     -- ==
     instance Eq CauchyList where
         CauchyList num1 list1 == CauchyList num2 list2
@@ -45,13 +45,13 @@ module Cauchy where
 
         CauchyList num1 list1 * CauchyList num2 list2 =
             CauchyList num1 [x `mod` num1 | x <- multiplyList list1 list2]
-        
+
         abs (CauchyList num list) = CauchyList num [abs x | x <- list]
 
         signum (CauchyList num list) = CauchyList num [signum x | x <- list]
 
         fromInteger n = CauchyList (fromIntegral n) [fromIntegral n]
-    
+
     -- Give a string that mimics the output. String representation
     instance Show CauchyList where
         show (CauchyList num list) =
--- final/Cauchy.rs
+++ final/Cauchy.rs
@@ -12,41 +12,104 @@ impl CauchyList {
 }
 
 impl cmp::PartialEq for CauchyList {
-    fn eq(&self, other: &Self) -> bool {    
-        // Implement == operation here
+    fn eq(&self, other: &Self) -> bool {
+        let length1 = self.content.len();
+        let length2 = other.content.len();
+        if (length1 == length2) && (self.p == other.p) {
+            for i in 0..length1 {
+                if self.content[i] != other.content[i] {
+                    return false;
+                }
+            }
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 }
 
 impl ops::Add<CauchyList> for CauchyList {
     type Output = CauchyList;
     fn add(self, other: CauchyList) -> CauchyList {
-        // Implement + operation here
+        let mut list = vec![0; cmp::max(self.content.len(), other.content.len())];
+        for i in 0..cmp::max(self.content.len(), other.content.len()) {
+            if i > (self.content.len() - 1) {
+                list[i] = (other.content[i]) % self.p
+            }
+            else if i > (other.content.len() - 1) {
+                list[i] = (self.content[i]) % self.p
+            }
+            else {
+                list[i] = (self.content[i] + other.content[i]) % self.p;
+            }
+        }
+        return CauchyList {p: self.p, content: list}
     }
 }
 
 impl ops::Sub<CauchyList> for CauchyList {
     type Output = CauchyList;
     fn sub(self, other: CauchyList) -> CauchyList {
-        // Implement - operation here
+        let mut list = vec![0; cmp::max(self.content.len(), other.content.len())];
+        for i in 0..cmp::max(self.content.len(), other.content.len()) {
+            if i > (self.content.len() - 1) {
+                list[i] = (other.content[i]) % self.p
+            }
+            else if i > (other.content.len() - 1) {
+                list[i] = (self.content[i]) % self.p
+            }
+            else {
+                list[i] = (self.content[i] - other.content[i]) % self.p;
+            }
+        }
+        return CauchyList {p: self.p, content: list}
     }
 }
 
 impl ops::Mul<CauchyList> for CauchyList {
     type Output = CauchyList;
     fn mul(self, other: CauchyList) -> CauchyList {
-        // Implement * operation here
+        let mut answer = vec![0; self.content.len() + other.content.len() - 1];
+        let mut list1 = self.content;
+        let mut list2 = other.content;
+        let mut sample1 = vec![];
+        let mut sample2 = vec![];
+        let mut acc;
+        for i in 0..answer.len() {
+            if i > list1.len() - 1 {
+                list1.push(0);
+            }
+            if i > list2.len() - 1 {
+                list2.push(0);
+            }
+        }
+        for i in 0..answer.len() {
+            acc = 0;
+            sample1.push(list1[i]);
+            sample2.push(list2[i]);
+            for j in 0..sample1.len() {
+                acc += sample1[j] * sample2[sample2.len() - j - 1];
+            }
+            answer[i] = acc % self.p;
+        }
+        return CauchyList {p: self.p, content: answer}
     }
 }
 
 impl ops::Mul<i32> for CauchyList {
     type Output = CauchyList;
     fn mul(self, other: i32) -> CauchyList {
-        // Implement * operation here
+        let mut answer = vec![];
+        for i in 0..self.content.len() {
+            answer.push((self.content[i] * other) % self.p);
+        }
+        return CauchyList {p: self.p, content: answer}
     }
 }
 
 impl fmt::Display for CauchyList {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {    
-        // Implement print formatting here       
+        write!(f,"p: {}\nlength: {}\ncontents: {:?}\n", self.p, self.content.len(), self.content)
     }
 }
